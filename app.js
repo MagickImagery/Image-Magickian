2446009a80dfaf258cd02e3b7619cd70a34b30fa
Neuhaus <neuhaus87@gmail.com>
2015-07-08 18:58:43 -0400

    fire it up

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("MAGIC!!!")
+var express = require('express');
+var morgan  = require('morgan');
+var app     = express();
+var port    = 3000;
+
+app.use(morgan('combined'));
+app.use(express.static('public'));
+
+app.use(express.static('bower_components'));
+app.set('view engine', 'ejs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
+app.get('/', funciton(req, res){
+  res.render('index');
+})
+
+
+app.listen(port);
